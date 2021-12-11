b61bf5e2e31b8669f42fada4f9ab4891735ccec4
Faraz Shuja <farazshuja@gmail.com>
2021-12-11 11:39:37 +0500

    generated dist folder

---
+++ precache-manifest.d8fcad1efa6ea03a1b6df99fa8e7be2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69fd33e74f8cb7e98d57",
+    "url": "/tuql/css/app.851cc563.css"
+  },
+  {
+    "revision": "83a9f3072a140d159afa",
+    "url": "/tuql/css/chunk-vendors.54bcddb4.css"
+  },
+  {
+    "revision": "4a774058c07bcae9e92d3c2a2218adf7",
+    "url": "/tuql/index.html"
+  },
+  {
+    "revision": "69fd33e74f8cb7e98d57",
+    "url": "/tuql/js/app.f620fdad.js"
+  },
+  {
+    "revision": "83a9f3072a140d159afa",
+    "url": "/tuql/js/chunk-vendors.de0dd75f.js"
+  },
+  {
+    "revision": "98605374fdb1430f9f874fe3b409a6f1",
+    "url": "/tuql/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/tuql/robots.txt"
+  }
+]);
